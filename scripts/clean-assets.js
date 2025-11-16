dc6de997b3780858e99f853f0e937959f5be21c1
kali-shade <okimkali@gmail.com>
2025-11-16 20:38:39 -0300

    spark.js interactive deform effect

--- scripts/clean-assets.js
+++
@@ -1,8 +0,0 @@
-import { existsSync } from "node:fs";
-import { rm } from "node:fs/promises";
-const assetsDirectory = "examples/assets/";
-
-if (existsSync(assetsDirectory)) {
-  await rm(assetsDirectory, { recursive: true, force: true });
-  console.log(`Directory ${assetsDirectory} deleted`);
-}
--- scripts/clean-site.js
+++
@@ -1,8 +0,0 @@
-import { existsSync } from "node:fs";
-import { rm } from "node:fs/promises";
-const siteDirectory = "site";
-
-if (existsSync(siteDirectory)) {
-  await rm(siteDirectory, { recursive: true, force: true });
-  console.log(`Directory ${siteDirectory} deleted`);
-}
